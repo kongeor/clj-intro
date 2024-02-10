e9a5b3bc6ff1750f85208f58770664185acc6e96
Kostas Georgiadis <kongeor@gmail.com>
2024-02-10 11:51:06 +0200

    alpha7 fun

--- src/clj_intro/interop.clj
+++ src/clj_intro/interop.clj
@@ -49,3 +49,66 @@
 ;; coercions
 
 ;; optimization
+
+
+;; -----------------
+;; ALPHA LAND!
+;; 1.12alpha6 stuff
+
+;; method values
+
+(comment
+  ;; does not work because of different arities
+  (map Integer/parseInt ["1" "2"]))
+
+(map ^[String] Integer/parseInt ["1" "2"])
+
+;; java field descriptors: /home/kostas/projects/clj-intro/src/clj_intro/interop.clj
+
+(defn str->bytes [s]
+  (bytes (byte-array (map byte s))))
+
+(str->bytes "hoho")
+
+;; pre alpha6 style
+
+(defn my-str-new-2 [^"[B" b]
+  (String. b))
+
+(my-str-new-2 (str->bytes "foo"))
+
+(defn my-str-new [b]
+  (^[byte*] String. b))
+
+(my-str-new (str->bytes "foo"))
+
+;; streams ...
+
+(java.util.Arrays/stream ^"[Ljava.lang.String;" (to-array ["foo" "bar"]))
+
+(-> (java.util.stream.Stream/of (to-array ["12.43" "1.123" "0.54"]))
+    (.map (reify java.util.function.Function
+            (apply [this x]
+              (parse-double x))))
+    ;; not working yet
+    ;; https://clojure.atlassian.net/browse/CLJ-2799
+    #_(.map ^[double] Math/round)
+    (.collect (java.util.stream.Collectors/toList)))
+
+;; seq interop
+
+(stream-seq! (java.util.stream.Stream/of (to-array ["12.43" "1.123" "0.54"])))
+
+;; why would you do that?
+;; performance ... see https://clojure.atlassian.net/browse/CLJ-2791
+;; Implement Spliterable on PersistentVector
+
+;; used to work but now more performant
+(-> (range 10)
+    (.stream)
+    (.mapToInt
+      (reify java.util.function.ToIntFunction
+        (applyAsInt [_ i]
+          (.intValue ^Long i))))
+    (.sum ))
+
